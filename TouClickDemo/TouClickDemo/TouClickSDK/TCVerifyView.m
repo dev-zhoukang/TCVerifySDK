5a841cc181e9dfe60602e4f6a9fd24218ce93783
ZK <dev.zhoukang@gmail.com>
2017-03-30 21:10:10 +0800

    add animation

--- TouClickDemo/TouClickDemo/TouClickSDK/TCVerifyView.m
+++ TouClickDemo/TouClickDemo/TouClickSDK/TCVerifyView.m
@@ -28,6 +28,7 @@
 }
 
 - (void)setup {
+    _submitBtn.clipsToBounds = true;
     _submitBtn.layer.cornerRadius = 15.f;
     _submitBtn.layer.borderColor = [UIColor blueColor].CGColor;
     _submitBtn.layer.borderWidth = 1.f;
@@ -39,6 +40,20 @@
     view.us_height = 270.f;
     view.us_width = 255.f;
     view.center = view.superview.center;
+    
+    view.layer.transform = CATransform3DMakeScale(1.2f, 1.2f, 1.0f);
+    view.alpha = 0.0f;
+    
+    [UIView animateWithDuration:.25
+                          delay:0.0 options:UIViewAnimationOptionCurveEaseOut | UIViewAnimationOptionBeginFromCurrentState
+                     animations:^{
+                         
+                         view.layer.transform = CATransform3DIdentity;
+                         view.alpha = 1.0f;
+                         
+                     } completion:^(BOOL finished) {
+                     }];
+    
     return view;
 }
 
